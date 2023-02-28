4efa68f109641800035eaf6b8bf34df748754c41
mykfrost <okellofrost@gmail.com>
2023-02-28 13:35:22 +0300

    form values

--- first.php
+++ first.php
@@ -9,7 +9,46 @@
 </head>
 
 <body>
+
     <p>This is standard HTML</p>
+    <?php
+    echo 'This was generated using PHP';
+    $file = $_SERVER["SCRIPT_FILENAME"];
+    $user = $_SERVER["HTTP_USER_AGENT"];
+    $server = $_SERVER["SERVER_SOFTWARE"];
+    //print the name of this script
+
+    echo "<p>You are running the file : <br/><b>$file<b></p> \n";
+
+    // print the user info
+    echo "<p>You are viewing the page : <br/><b>$user<b></p> \n";
+
+    //print server info 
+
+    echo "<p>You are running the server : <br/><b>$server<b></p> \n";
+
+    echo "<h2> I want to handle a form</h2>";
+
+    ?>
+    <form action="handle_form.php" method="post">
+        <fieldset>
+            <legend>Enter Your Information in the form below</legend>
+            <p><label>Name : <input type="text" name="name" size="20" maxlength="40"></label></p>
+            <p><label>Email : <input type="text" name="email" size="20" maxlength="60"></label></p>
+            <p><label for="gender">Gender </label> : <input type="radio" name="gender" value="M">Male
+                <input type="radio" name="gender" value="F">Female
+            </p>
+            <p><label>Age:
+                    <select name="age">
+                        <option value="0-29">Under 30</option>
+                        <option value="30-60">Between 30 and 60</option>
+                        <option value="60+">Over 60 </option>
+                    </select>
+                </label>
+            </p>
+        </fieldset>
+        <p><input type="submit" name="submit" value="Submit My Informatation" /></p>
+    </form>
 </body>
 
 </html>
